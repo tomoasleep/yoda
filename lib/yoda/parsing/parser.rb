11e8d73e9a7605c0b781bd15735a7cf85a1a7b18
Tomoya Chiba <tomo.asleep@gmail.com>
2018-01-20 16:15:19 +0900

    Add Parser#parse_with_comments

--- lib/yoda/parsing/parser.rb
+++ lib/yoda/parsing/parser.rb
@@ -8,6 +8,20 @@ module Yoda
       def parse(string)
         ::Parser::CurrentRuby.parse(string)
       end
+
+      # @param string [String]
+      # @return [(::Parser::AST::Node, Array<::Parser::Source::Comment>)]
+      def parse_with_comments(string)
+        ::Parser::CurrentRuby.parse_with_comments(string)
+      end
+
+      # @param string [String]
+      # @return [(::Parser::AST::Node, Array<::Parser::Source::Comment>), nil]
+      def parse_with_comments_if_valid(string)
+        parse_with_comments(source)
+      rescue ::Parser::SyntaxError
+        nil
+      end
     end
   end
 end
