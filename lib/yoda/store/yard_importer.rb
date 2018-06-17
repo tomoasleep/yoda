7a0a83cc9b55f63a954a909c5d27a2e063eabd15
Tomoya Chiba <tomo.asleep@gmail.com>
2018-06-18 02:20:48 +0900

    Suppose proxy object is directly under its namespace

--- lib/yoda/store/yard_importer.rb
+++ lib/yoda/store/yard_importer.rb
@@ -73,26 +73,33 @@ module Yoda
       # @param code_object [::YARD::CodeObjects::NamespaceObject]
       # @return [Objects::ClassObject]
       def convert_root_object(code_object)
-        # @todo Add meta class for main object.
-        Objects::ClassObject.new(
-          path: 'Object',
+        object_class = Objects::ClassObject.new(
+          path: path_to_store(code_object),
           document: code_object.docstring.to_s,
           tag_list: code_object.tags.map { |tag| convert_tag(tag, '') },
           sources: code_object.files.map(&method(:convert_source)),
           primary_source: code_object[:current_file_has_comments] ? convert_source(code_object.files.first) : nil,
           instance_method_addresses: code_object.meths(included: false, scope: :instance).map(&:path),
-          mixin_addresses: code_object.instance_mixins.map { |mixin| mixin.path },
+          mixin_addresses: code_object.instance_mixins.map { |mixin| path_to_store(mixin) },
           constant_addresses: (code_object.children.select{ |child| %i(constant module class).include?(child.type) }.map { |constant| constant.path } + ['Object']).uniq,
         )
+        object_meta_class = Objects::MetaClassObject.new(
+          path: path_to_store(code_object),
+          sources: code_object.files.map(&method(:convert_source)),
+          primary_source: code_object[:current_file_has_comments] ? convert_source(code_object.files.first) : nil,
+          instance_method_addresses: code_object.meths(included: false, scope: :class).map(&:path),
+          mixin_addresses: code_object.instance_mixins.map { |mixin| path_to_store(mixin) },
+        )
+        [object_class, object_meta_class]
       end
 
       # @param code_object [::YARD::CodeObjects::ConstantObject]
       # @return [Objects::ValueObject]
       def convert_constant_object(code_object)
         Objects::ValueObject.new(
-          path: code_object.path,
+          path: path_to_store(code_object),
           document: code_object.docstring.to_s,
-          tag_list: code_object.tags.map { |tag| convert_tag(tag, code_object.namespace.path) },
+          tag_list: code_object.tags.map { |tag| convert_tag(tag, path_to_store(code_object.namespace)) },
           sources: code_object.files.map(&method(:convert_source)),
           primary_source: code_object[:current_file_has_comments] ? convert_source(code_object.files.first) : nil,
           value: code_object.value,
@@ -107,8 +114,8 @@ module Yoda
           method_object = Objects::MethodObject.new(
             path: "Object#{code_object.sep}#{code_object.name}",
             document: code_object.docstring.to_s,
-            tag_list: code_object.tags.map { |tag| convert_tag(tag, code_object.namespace.path) },
-            overloads: code_object.tags(:overload).map { |tag| convert_overload_tag(tag, code_object.namespace.path) },
+            tag_list: code_object.tags.map { |tag| convert_tag(tag, path_to_store(code_object.namespace)) },
+            overloads: code_object.tags(:overload).map { |tag| convert_overload_tag(tag, path_to_store(code_object.namespace)) },
             sources: code_object.files.map(&method(:convert_source)),
             primary_source: code_object[:current_file_has_comments] ? convert_source(code_object.files.first) : nil,
             parameters: code_object.parameters,
@@ -121,10 +128,10 @@ module Yoda
           [method_object, object_object]
         else
           Objects::MethodObject.new(
-            path: code_object.path,
+            path: path_to_store(code_object),
             document: code_object.docstring.to_s,
-            tag_list: code_object.tags.map { |tag| convert_tag(tag, code_object.namespace.path) },
-            overloads: code_object.tags(:overload).map { |tag| convert_overload_tag(tag, code_object.namespace.path) },
+            tag_list: code_object.tags.map { |tag| convert_tag(tag, path_to_store(code_object.namespace)) },
+            overloads: code_object.tags(:overload).map { |tag| convert_overload_tag(tag, path_to_store(code_object.namespace)) },
             sources: code_object.files.map(&method(:convert_source)),
             primary_source: code_object[:current_file_has_comments] ? convert_source(code_object.files.first) : nil,
             parameters: code_object.parameters,
@@ -137,22 +144,22 @@ module Yoda
       # @return [Array<Objects::ModuleObject, Objects::MetaClassObject>]
       def convert_module_object(code_object)
         module_object = Objects::ModuleObject.new(
-          path: code_object.path,
+          path: path_to_store(code_object),
           document: code_object.docstring.to_s,
-          tag_list: code_object.tags.map { |tag| convert_tag(tag, code_object.path) },
+          tag_list: code_object.tags.map { |tag| convert_tag(tag, path_to_store(code_object)) },
           sources: code_object.files.map(&method(:convert_source)),
           primary_source: code_object[:current_file_has_comments] ? convert_source(code_object.files.first) : nil,
           instance_method_addresses: code_object.meths(included: false, scope: :instance).map(&:path),
-          mixin_addresses: code_object.instance_mixins.map { |mixin| mixin.path },
+          mixin_addresses: code_object.instance_mixins.map { |mixin| path_to_store(mixin) },
           constant_addresses: code_object.children.select{ |child| %i(constant module class).include?(child.type) }.map { |constant| constant.path },
         )
 
         meta_class_object = Objects::MetaClassObject.new(
-          path: code_object.path,
+          path: path_to_store(code_object),
           sources: code_object.files.map(&method(:convert_source)),
           primary_source: code_object[:current_file_has_comments] ? convert_source(code_object.files.first) : nil,
           instance_method_addresses: code_object.meths(included: false, scope: :class).map(&:path),
-          mixin_addresses: code_object.instance_mixins.map { |mixin| mixin.path },
+          mixin_addresses: code_object.instance_mixins.map { |mixin| path_to_store(mixin) },
         )
 
         [module_object, meta_class_object]
@@ -162,23 +169,23 @@ module Yoda
       # @return [Array<Objects::ClassObject, Objects::MetaClassObject>]
       def convert_class_object(code_object)
         class_object = Objects::ClassObject.new(
-          path: code_object.path,
+          path: path_to_store(code_object),
           document: code_object.docstring.to_s,
-          tag_list: code_object.tags.map { |tag| convert_tag(tag, code_object.path) },
+          tag_list: code_object.tags.map { |tag| convert_tag(tag, path_to_store(code_object)) },
           sources: code_object.files.map(&method(:convert_source)),
           primary_source: code_object[:current_file_has_comments] ? convert_source(code_object.files.first) : nil,
           instance_method_addresses: code_object.meths(included: false, scope: :instance).map(&:path),
-          mixin_addresses: code_object.instance_mixins.map { |mixin| mixin.path },
-          constant_addresses: code_object.children.select{ |child| %i(constant module class).include?(child.type) }.map { |constant| constant.path },
-          superclass_path: code_object.superclass&.path == 'Qnil' ? nil : code_object.superclass&.path,
+          mixin_addresses: code_object.instance_mixins.map { |mixin| path_to_store(mixin) },
+          constant_addresses: code_object.children.select{ |child| %i(constant module class).include?(child.type) }.map { |constant| path_to_store(constant) },
+          superclass_path: !code_object.superclass || code_object.superclass&.path == 'Qnil' ? nil : path_to_store(code_object.superclass),
         )
 
         meta_class_object = Objects::MetaClassObject.new(
-          path: code_object.path,
+          path: path_to_store(code_object),
           sources: code_object.files.map(&method(:convert_source)),
           primary_source: code_object[:current_file_has_comments] ? convert_source(code_object.files.first) : nil,
           instance_method_addresses: code_object.meths(included: false, scope: :class).map(&:path),
-          mixin_addresses: code_object.class_mixins.map { |mixin| mixin.path },
+          mixin_addresses: code_object.class_mixins.map { |mixin| path_to_store(mixin) },
         )
 
         [class_object, meta_class_object]
@@ -220,7 +227,7 @@ module Yoda
       # @return [Array<Objects::ModuleObject>]
       def create_proxy_module(code_object)
         module_object = Objects::ModuleObject.new(
-          path: code_object.path,
+          path: path_to_store(code_object),
           document: '',
           tag_list: [],
           sources: [],
@@ -231,7 +238,7 @@ module Yoda
         )
 
         meta_class_object = Objects::MetaClassObject.new(
-          path: code_object.path,
+          path: path_to_store(code_object),
           sources: [],
           primary_source: nil,
           instance_method_addresses: [],
@@ -244,9 +251,9 @@ module Yoda
       # @param code_object [::YARD::CodeObjects::Base]
       # @return [vaid]
       def register_to_parent_proxy(code_object)
-        proxy_module = patch.find(code_object.parent.path)
-        proxy_module.instance_method_addresses.push(code_object.path) if code_object.type == :method
-        proxy_module.constant_addresses.push(code_object.path) if [:class, :module, :proxy].include?(code_object.type)
+        proxy_module = patch.find(path_to_store(code_object.parent))
+        proxy_module.instance_method_addresses.push(path_to_store(code_object)) if code_object.type == :method
+        proxy_module.constant_addresses.push(path_to_store(code_object)) if [:class, :module, :proxy].include?(code_object.type)
       end
 
       # @param source [(String, Integer)]
@@ -254,6 +261,29 @@ module Yoda
       def convert_source(source)
         file, line = source
         [root_path ? File.expand_path(file, root_path) : file, line, 0]
+      end
+
+      # @param code_object [::YARD::CodeObjects::Base]
+      # @return [String]
+      def path_to_store(object)
+        @paths_to_store ||= {}
+        @paths_to_store[[object.type, object.path]] ||= calc_path_to_store(object)
+      end
+
+      # @param code_object [::YARD::CodeObjects::Base]
+      # @return [String]
+      def calc_path_to_store(object)
+        return 'Object' if object.root?
+        parent_path = path_to_store(object.parent)
+
+        if object.type == :proxy || object.is_a?(YARD::CodeObjects::Proxy)
+          # For now, we suppose the proxy object exists directly under its lexical namespace.
+          [path_to_store(object.parent), object.name].join('::')
+        elsif object.parent.path == path_to_store(object.parent)
+          object.path
+        else
+          [path_to_store(object.parent), object.name].join(object.sep)
+        end.gsub(/^Object::/, '')
       end
     end
   end
