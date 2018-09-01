531fa215fdea34514a4f32af95e5dfe099833956
Tomoya Chiba <tomo.asleep@gmail.com>
2018-09-01 22:44:00 +0900

    Make Registry thread safe

--- lib/yoda/store/registry.rb
+++ lib/yoda/store/registry.rb
@@ -1,3 +1,4 @@
+require 'concurrent'
 require 'yard'
 
 module Yoda
@@ -5,12 +6,6 @@ module Yoda
     class Registry
       # @note This number must be updated when breaking change is added.
       REGISTRY_VERSION = 1
-
-      # @return [Adapters::LmdbAdapter, nil]
-      attr_reader :adapter
-
-      # @return [Objects::PatchSet]
-      attr_reader :patch_set
 
       PROJECT_STATUS_KEY = '%project_status'
 
@@ -21,33 +16,43 @@ module Yoda
 
       # @return [Objects::ProjectStatus, nil]
       def project_status
-        adapter&.exist?(PROJECT_STATUS_KEY) && adapter.get(PROJECT_STATUS_KEY)
+        lock.with_read_lock do
+          adapter&.exist?(PROJECT_STATUS_KEY) && adapter.get(PROJECT_STATUS_KEY)
+        end
       end
 
       # @param new_project_status [Objects::ProjectStatus]
       def save_project_status(new_project_status)
-        adapter.put(PROJECT_STATUS_KEY, new_project_status)
+        lock.with_write_lock do
+          adapter.put(PROJECT_STATUS_KEY, new_project_status)
+        end
       end
 
       # @param path [String]
       # @return [Objects::Base, nil]
       def find(path)
-        if adapter&.exist?(path)
-          patch_set.patch(adapter.get(path))
-        else
-          patch_set.find(path)
+        lock.with_read_lock do
+          if adapter&.exist?(path)
+            patch_set.patch(adapter.get(path))
+          else
+            patch_set.find(path)
+          end
         end
       end
 
       # @param patch [Patch]
       def add_patch(patch)
-        patch_set.register(patch)
+        lock.with_write_lock do
+          patch_set.register(patch)
+        end
       end
 
       # @param path [String]
       # @return [true, false]
       def has_key?(path)
-        adapter&.exists?(path) || patch_set.has_key?(path)
+        lock.with_read_lock do
+          adapter&.exists?(path) || patch_set.has_key?(path)
+        end
       end
 
       def clear
@@ -60,20 +65,33 @@ module Yoda
       # old data in the database are discarded.
       def compress_and_save
         return unless adapter
-        el_keys = patch_set.keys
-        progress = Instrument::Progress.new(el_keys.length) { |length:, index:| Instrument.instance.registry_dump(index: index, length: length) }
+        lock.with_write_lock do
+          el_keys = patch_set.keys
+          progress = Instrument::Progress.new(el_keys.length) { |length:, index:| Instrument.instance.registry_dump(index: index, length: length) }
 
-        data = Enumerator.new do |yielder|
-          el_keys.each { |key| yielder << [key, patch_set.find(key)] }
+          data = Enumerator.new do |yielder|
+            el_keys.each { |key| yielder << [key, patch_set.find(key)] }
+          end
+
+          adapter.batch_write(data, progress)
+          adapter.sync
+          Logger.info "saved #{el_keys.length} keys."
+          @patch_set = Objects::PatchSet.new
         end
-
-        adapter.batch_write(data, progress)
-        adapter.sync
-        Logger.info "saved #{el_keys.length} keys."
-        @patch_set = Objects::PatchSet.new
       end
 
       private
+
+      # @return [Adapters::LmdbAdapter, nil]
+      attr_reader :adapter
+
+      # @return [Objects::PatchSet]
+      attr_reader :patch_set
+
+      # @return [Concurrent::ReentrantReadWriteLock]
+      def lock
+        @lock ||= Concurrent::ReentrantReadWriteLock.new
+      end
 
       def keys
         Set.new(adapter&.keys.map(&:to_s) || []).union(patch_set.keys)
