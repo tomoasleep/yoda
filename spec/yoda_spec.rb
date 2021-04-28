1cf2474e4fcc48fa8070bc7649ff06dfdf20f515
Tomoya Chiba <tomo.asleep@gmail.com>
2021-04-28 10:17:39 +0900

    Add cli spec to read source

--- spec/yoda_spec.rb
+++ spec/yoda_spec.rb
@@ -8,9 +8,26 @@ RSpec.describe Yoda do
   describe "server" do
     include ServerHelper
 
-    let(:command) { "#{executable} server" }
+    let(:command) { "#{executable} server --log-level=trace" }
 
     it "can launch" do
+      messages, error, status = capture_server(command: command, fixture_path: fixture_path) do |requests|
+        requests << lsp_request(
+          id: "test",
+          method: :initialize,
+          params: {
+            process_id: nil,
+            root_uri: fixture_path,
+            capabilities: lsp(:client_capabilities),
+          }
+        )
+      end
+
+      puts error
+      expect(messages[0]).not_to have_key(:error)
+    end
+
+    it "can open file" do
       messages, error, status = capture_server(command: command, fixture_path: fixture_path) do |requests|
         requests << lsp_request(
           id: "test",
@@ -21,10 +38,35 @@ RSpec.describe Yoda do
             capabilities: lsp(:client_capabilities),
           }
         )
+
+        requests << lsp_request(
+          id: "test-new",
+          method: :initialized,
+          params: {}
+        )
+
+        sample_path = File.expand_path('./lib/sample.rb', fixture_path)
+
+        requests << lsp_request(
+          id: "test-new",
+          method: :'textDocument/didOpen',
+          kind: :'did_open_text_document',
+          params: {
+            text_document: lsp(
+              :text_document_item,
+              uri: "file://#{sample_path}",
+              language_id: "ruby",
+              version: 1,
+              text: File.read(sample_path),
+            ),
+          }
+        )
       end
 
       puts error
-      expect(messages[0]).not_to have_key(:error)
+      messages.each do |message|
+        expect(message).not_to have_key(:error)
+      end
     end
 
 
