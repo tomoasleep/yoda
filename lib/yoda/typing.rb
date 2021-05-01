4a7e78ce02aa2cf52fa4195cc9a21b7915880ce1
Tomoya Chiba <tomo.asleep@gmail.com>
2021-05-01 19:10:05 +0900

    Remove unused modules

--- lib/yoda/typing.rb
+++ lib/yoda/typing.rb
@@ -1,8 +1,6 @@
 module Yoda
   module Typing
-    require 'yoda/typing/context'
     require 'yoda/typing/environment'
-    require 'yoda/typing/evaluator'
     require 'yoda/typing/relation'
     require 'yoda/typing/traces'
     require 'yoda/typing/lexical_scope'
