604e23fb66446b9ce4ea2e8a6177da3139e9d05d
Tomoya Chiba <tomo.asleep@gmail.com>
2022-01-01 13:38:57 +0900

    Show command help by option

--- lib/yoda/cli.rb
+++ lib/yoda/cli.rb
@@ -12,41 +12,48 @@ module Yoda
     class Top < Thor
       class_option :log_level, type: :string, desc: 'Set log level (debug info warn error fatal)'
       class_option :profile, type: :boolean, desc: 'Set log level (debug info warn error fatal)'
+      class_option :help, type: :boolean, desc: 'Show help', aliases: :h,  hide: true
 
       desc 'setup', 'Setup indexes for current Ruby version and project gems'
       option :force_build, type: :boolean, desc: "If enabled, (re)build current project's index forcibly"
       def setup
         process_class_options
+        return self.class.command_help(shell, __method__) if options[:help]
         Store.setup(dir: Dir.pwd, force_build: options[:force_build])
       end
 
       desc 'infer POSITION', 'Infer the type of value at the specified position'
       def infer(position)
         process_class_options
+        return self.class.command_help(shell, __method__) if options[:help]
         Cli::Infer.run(position)
       end
 
       desc 'complete POSITION', 'Provide completion candidates for the specified position'
       def complete(position)
         process_class_options
+        return self.class.command_help(shell, __method__) if options[:help]
         Cli::Complete.run(position)
       end
 
       desc 'console', 'Launch debug console'
       def console
         process_class_options
+        return self.class.command_help(shell, __method__) if options[:help]
         Cli::Console.run
       end
 
       desc 'server', 'Start Language Server'
       def server
         process_class_options
+        return self.class.command_help(shell, __method__) if options[:help]
         Server.new.run
       end
 
       desc 'version', 'show current version'
       def version
         process_class_options
+        return self.class.command_help(shell, __method__) if options[:help]
         say "#{Yoda::VERSION}"
       end
 
