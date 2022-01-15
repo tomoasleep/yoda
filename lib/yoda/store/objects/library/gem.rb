ab4f42ded929541fa3bcef662479c47bcad4d5d7
Tomoya Chiba <tomo.asleep@gmail.com>
2022-01-15 17:11:15 +0900

    Support not installed gems

--- lib/yoda/store/objects/library/gem.rb
+++ lib/yoda/store/objects/library/gem.rb
@@ -13,13 +13,25 @@ module Yoda
             # @param spec [Bundler::LazySpecification]
             def from_gem_spec(spec)
               fail "#{spec} is not installed" unless spec.respond_to?(:full_gem_path)
-              new(
-                name: spec.name,
-                version: spec.version.version,
-                source_path: spec.source.respond_to?(:path) ? spec.source.path : nil,
-                full_gem_path: spec.full_gem_path,
-                doc_dir: spec.doc_dir,
-              )
+              if spec.respond_to?(:full_gem_path)
+                # Installed
+                new(
+                  name: spec.name,
+                  version: spec.version.version,
+                  source_path: spec.source.respond_to?(:path) ? spec.source.path : nil,
+                  full_gem_path: spec.full_gem_path,
+                  doc_dir: spec.doc_dir,
+                )
+              else
+                # Not installed
+                new(
+                  name: spec.name,
+                  version: spec.version.version,
+                  source_path: nil,
+                  full_gem_path: nil,
+                  doc_dir: nil,
+                )
+              end
             end
           end
 
@@ -57,6 +69,11 @@ module Yoda
           # @note Implementation for {WithRegistry#registry_path}
           def registry_path
             VersionStore.for_current_version.registry_path_for_gem(name: name, version: version)
+          end
+
+          # @return [Boolean]
+          def installed?
+            !!full_gem_path
           end
         end
       end
