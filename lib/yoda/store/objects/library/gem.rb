fc3e0ebc149ed4b9dbd02314e0a3990021fe41bb
Tomoya Chiba <tomo.asleep@gmail.com>
2022-01-15 17:12:24 +0900

    Add source type

--- lib/yoda/store/objects/library/gem.rb
+++ lib/yoda/store/objects/library/gem.rb
@@ -9,6 +9,9 @@ module Yoda
           # @return [String]
           attr_reader :name, :version, :source_path, :full_gem_path, :doc_dir
 
+          # @return [Symbol, nil]
+          attr_reader :source_type
+
           class << self
             # @param spec [Bundler::LazySpecification]
             def from_gem_spec(spec)
@@ -21,6 +24,7 @@ module Yoda
                   source_path: spec.source.respond_to?(:path) ? spec.source.path : nil,
                   full_gem_path: spec.full_gem_path,
                   doc_dir: spec.doc_dir,
+                  source_type: source_type_of(spec.source),
                 )
               else
                 # Not installed
@@ -30,18 +34,39 @@ module Yoda
                   source_path: nil,
                   full_gem_path: nil,
                   doc_dir: nil,
+                  source_type: nil,
                 )
+              end
+            end
+
+            # @param source [Bundler::Source, nil]
+            # @return [Symbol, nil]
+            def source_type_of(source)
+              return nil unless source
+
+              case source
+              when Bundler::Source::Git
+                :git
+              when Bundler::Source::Gemspec
+                :gemspec
+              when Bundler::Source::Rubygems
+                :rubygems
+              when Bundler::Source::Path
+                :path
+              else
+                nil
               end
             end
           end
 
           # @param spec [Bundler::LazySpecification]
-          def initialize(name:, version:, source_path:, full_gem_path:, doc_dir:)
+          def initialize(name:, version:, source_path:, full_gem_path:, doc_dir:, source_type:)
             @name = name
             @version = version
             @source_path = source_path
             @full_gem_path = full_gem_path
             @doc_dir = doc_dir
+            @source_type = source_type&.to_sym
           end
 
           def id
@@ -63,6 +88,7 @@ module Yoda
               source_path: source_path,
               full_gem_path: full_gem_path,
               doc_dir: doc_dir,
+              source_type: source_type,
             }
           end
 
@@ -74,6 +100,10 @@ module Yoda
           # @return [Boolean]
           def installed?
             !!full_gem_path
+          end
+
+          def managed_by_rubygems?
+            source_type == :rubygems
           end
         end
       end
