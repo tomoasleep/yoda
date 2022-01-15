d74d61985c9b0bbf5c460c511fa63498465f7f95
Tomoya Chiba <tomo.asleep@gmail.com>
2022-01-15 17:04:21 +0900

    Allow nil for parameter node

--- lib/yoda/ast/parameter_node.rb
+++ lib/yoda/ast/parameter_node.rb
@@ -1,14 +1,14 @@
 module Yoda
   module AST
     class ParameterNode < Node
-      # @return [NameVnode, EmptyNode]
+      # @return [NameVnode, EmptyNode, nil]
       def content
         children[0]
       end
 
       # @return [Model::Parameters::Base]
       def parameter
-        content.present? ? Model::Parameters::Named.new(content.name) : Model::Parameters::Unnamed.new
+        content&.present? ? Model::Parameters::Named.new(content.name) : Model::Parameters::Unnamed.new
       end
 
       # @return [Model::Parameters::Base]
