2cfecc988581ff7ed508b3bf1fe8c02b180a8efe
Tomoya Chiba <tomo.asleep@gmail.com>
2022-02-06 11:42:49 +0900

    Remove unused SourceAnalyzer

--- lib/yoda/parsing.rb
+++ lib/yoda/parsing.rb
@@ -3,7 +3,6 @@ module Yoda
     require 'yoda/parsing/ast_traversable'
     require 'yoda/parsing/comment_tokenizer'
     require 'yoda/parsing/parser'
-    require 'yoda/parsing/source_analyzer'
     require 'yoda/parsing/node_objects'
     require 'yoda/parsing/location'
     require 'yoda/parsing/scopes'
