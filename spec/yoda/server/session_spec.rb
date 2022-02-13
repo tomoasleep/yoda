1d3d2d0754c1cafc625d3138f3e02f5b1c99d5c2
Tomoya Chiba <tomo.asleep@gmail.com>
2022-02-13 21:47:22 +0900

    Add session spec

---
+++ spec/yoda/server/session_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+RSpec.describe Yoda::Server::Session do
+  include FileUriHelper
+
+  let(:session) { described_class.from_root_uri(root_uri) }
+  let(:root_uri) { fixture_root_uri }
+
+  describe '#workspaces_for' do
+    subject { session.workspaces_for(uri) }
+
+    let(:uri) { file_uri('lib/sample.rb') }
+
+    it "returns workspaces for the given uri" do
+      expect(subject).to contain_exactly(
+        have_attributes(root_uri: root_uri),
+      )
+    end
+  end
+end
