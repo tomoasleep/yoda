5e7596c45daadab986d42cf276c678d37d927f12
Tomoya Chiba <tomo.asleep@gmail.com>
2022-03-22 00:16:58 +0900

    Enable progressOnInitialization for vscode

--- client/vscode/src/language-server.ts
+++ client/vscode/src/language-server.ts
@@ -19,6 +19,7 @@ export function configureLanguageServer(): LanguageClient {
     }
 
     const clientOptions : LanguageClientOptions = {
+        progressOnInitialization: true,
         documentSelector: [{ scheme: 'file', language: 'ruby' }],
         synchronize: {
             configurationSection: 'yoda',
