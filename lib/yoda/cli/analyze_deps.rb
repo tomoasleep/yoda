5c529f68a030d2a8f302a5c8677359d155bf98ed
Tomoya Chiba <tomo.asleep@gmail.com>
2022-03-26 23:53:18 +0900

    Return dependency ids to autoload

--- lib/yoda/cli/analyze_deps.rb
+++ lib/yoda/cli/analyze_deps.rb
@@ -16,7 +16,7 @@ module Yoda
 
         # @param root_path [String]
         def initialize(root_path)
-          @root_path = root_path
+          @root_path = File.expand_path(root_path)
         end
 
         # @return [Array<Yoda::Store::Objects::Library::Gem>]
@@ -28,6 +28,7 @@ module Yoda
           {
             path: root_path,
             dependencies: gems.map(&:to_h),
+            autoload_dependency_ids: autoload_gem_ids,
           }
         end
 
@@ -45,37 +46,45 @@ module Yoda
           end
         end
 
-        # @return [Array<Bundler::LazySpecification>, nil]
+        # @return [Array<String>]
+        def autoload_gem_ids
+          if has_gemfile?
+            gems.map(&:id)
+          else
+            []
+          end
+        end
+
+        # @return [Array<Bundler::LazySpecification, Gem::Specification>, nil]
         def gem_specs
           @gem_specs ||= begin
-            Dir.chdir(root_path) do
-              with_unbundled_env do
-                # Suppress bundler outputs to stdout.
-                Bundler.ui = Bundler::UI::Silent.new
+            with_project_env do
+              if has_gemfile?
+                # Resolve dependencies of uninstalled gems and ensure remote sources are available.
+                Bundler.definition.resolve_remotely!
+                spec_set = Bundler.definition.resolve
 
-                Bundler.reset!
-
-                if File.exists?("Gemfile")
-                  # Resolve dependencies of uninstalled gems and ensure remote sources are available.
-                  Bundler.definition.resolve_remotely!
-                  spec_set = Bundler.definition.resolve
-
-                  if Gem::Version.new(Bundler::VERSION) >= Gem::Version.new('2.2.25')
-                    deps = Bundler.definition.requested_dependencies
-                    spec_set.materialize(deps).to_a
-                  else
-                    # For backward compatibility (Ref: https://github.com/rubygems/rubygems/pull/4788)
-                    deps = Bundler.definition.send(:requested_dependencies)
-                    missing = []
-                    materialized = spec_set.materialize(deps, missing)
-                    materialized.to_a + missing
-                  end
+                if Gem::Version.new(Bundler::VERSION) >= Gem::Version.new('2.2.25')
+                  deps = Bundler.definition.requested_dependencies
+                  spec_set.materialize(deps).to_a
                 else
-                  []
+                  # For backward compatibility (Ref: https://github.com/rubygems/rubygems/pull/4788)
+                  deps = Bundler.definition.send(:requested_dependencies)
+                  missing = []
+                  materialized = spec_set.materialize(deps, missing)
+
+                  materialized.to_a + missing
                 end
+              else
+                [] # Gem::Specification.latest_specs(true)
               end
             end
           end
+        end
+
+        # @return [Boolean]
+        def has_gemfile?
+          File.exists?(File.expand_path("Gemfile", root_path))
         end
 
         # @param [Gem::Specification]
@@ -86,6 +95,18 @@ module Yoda
         # @param [Gem::Specification]
         def self_spec?(spec)
           spec.source.is_a?(Bundler::Source::Path) && (File.expand_path(spec.source.path) == File.expand_path(root_path))
+        end
+
+        def with_project_env
+          Dir.chdir(root_path) do
+            with_unbundled_env do
+              # Suppress bundler outputs to stdout.
+              Bundler.ui = Bundler::UI::Silent.new
+              Bundler.reset!
+
+              yield
+            end
+          end
         end
 
         def with_unbundled_env(&block)
